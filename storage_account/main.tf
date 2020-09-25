c274ee57101ca9c63e3a77aeb84f5cddab4d3ac5
Azahast <weyderfs@gmail.com>
2020-09-25 11:31:33 -0300

    hotfix/develop - Turning the module vars agnostic

--- storage_account/main.tf
+++ storage_account/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_storage_account" "asa" {
-  name                     = var.asa_name
-  resource_group_name      = var.asa_resource_group_name
-  location                 = var.asa_location
-  account_tier             = var.asa_account_tier
-  account_replication_type = var.asa_account_replication_type
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 
 }
--- storage_account/vars.tf
+++ storage_account/vars.tf
@@ -1,24 +1,24 @@
-  variable "asa_name" {
+  variable "name" {
       type          = string
       default       = null
       description   = "Name of Storage Account"
   }
-  variable "asa_resource_group_name" {
+  variable "resource_group_name" {
       type          = string
       default       = null
       description   = "Name of Resource Rroup"
   }
-  variable "asa_location" {
+  variable "location" {
       type          = string
       default       = null
       description   = "Geo Location"
   }
-  variable "asa_account_tier" {
+  variable "account_tier" {
       type          = string
       default       = null
       description   = "(Required) Defines the Tier to use for this storage account. Valid options are Standard and Premium. For BlockBlobStorage and FileStorage accounts only Premium is valid. Changing this forces a new resource to be created."
   }
-  variable "asa_account_replication_type" {
+  variable "account_replication_type" {
       type          = string
       default       = "GRS"
       description   = "(Required) Defines the type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS, ZRS, GZRS and RAGZRS."
