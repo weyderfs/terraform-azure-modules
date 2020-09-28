43478381a4ef11decd7309eddbf419aea80585b4
Azahast <weyderfs@gmail.com>
2020-09-28 17:06:09 -0300

    feature/develop - optimize vars

--- storage_account/vars.tf
+++ storage_account/vars.tf
@@ -15,7 +15,7 @@
   }
   variable "account_tier" {
       type          = string
-      default       = null
+      default       = "Standard"
       description   = "(Required) Defines the Tier to use for this storage account. Valid options are Standard and Premium. For BlockBlobStorage and FileStorage accounts only Premium is valid. Changing this forces a new resource to be created."
   }
   variable "account_replication_type" {
